ff8e0d2955fae082fa593cc108b8b71276aa47ef
Lubov959 <lubov.verigina@yandex.ru>
2025-04-28 11:56:52 +0300

    тест2

--- run.py
+++ run.py
@@ -5,3 +5,8 @@ from dotenv import load_dotenv
 load_dotenv('.venv')
 
 application = create_app()
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))  # Получаем порт из переменной окружения или используем 5000 по умолчанию
+    application.run(host='0.0.0.0', port=port)  # Указываем хост и порт
